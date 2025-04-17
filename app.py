4958c0359b6ebde633f337eb7c4d71e42d53c35b
Vetri170604 <vetrikumar6380268095@gmail.com>
2025-04-17 18:55:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def register():
         send_email("New User Registered", f"{email} just registered!", OFFICIAL_EMAIL)
 
         flash("✅ Registered successfully. Please login now.")
-        return redirect(url_for('login'))  # 🔁 Only this line changed
+        return redirect(url_for('register'))
 
     return render_template("register.html")
 
@@ -158,4 +158,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-s
